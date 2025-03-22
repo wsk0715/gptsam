60e10a20b991d7e3c8b9bbd6694677c1f066b7f8
wsk0715 <leindey@gmail.com>
2025-03-22 22:18:32 +0900

    :sparkles:feat: 공통 api 제공 모듈 생성(REST)

---
+++ frontend/src/app/api/apiClient.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import api from './axiosConfig';
+
+// 응답 body 타입 정의
+export type ApiResponse<T = void> = T;
+
+class ApiClient {
+	private readonly apiEndPoint = ''; // api 기본 엔드포인트
+
+	// GET 요청
+	async get<T = void>(url: string) {
+		try {
+			const response = await api.get<T>(this.apiEndPoint + url);
+			return response.data;
+		} catch (err) {
+			if (err instanceof Error) {
+				throw new Error(`요청 처리 중 에러가 발생했습니다: ${err.message}`);
+			}
+			throw new Error('요청 처리 중 예외가 발생했습니다.');
+		}
+	}
+}
+
+// 인스턴스 생성 및 내보내기
+export const apiClient = new ApiClient();
