732ffc794e4b13b2fba852d9787396cc6313737b
wsk0715 <leindey@gmail.com>
2025-03-23 21:17:20 +0900

    :sparkles:feat(Credential): 리프레시 토큰 함께 담도록 수정

--- backend/src/main/java/com/gptsam/core/credential/dto/Credential.java
+++ backend/src/main/java/com/gptsam/core/credential/dto/Credential.java
@@ -4,7 +4,16 @@ package com.gptsam.core.credential.dto;
  * 클라이언트 인증 정보를 담은 DTO
  */
 public record Credential(
-		String token
+		String token,
+		String refreshToken
 ) {
+
+	public static Credential of(String token) {
+		return new Credential(token, null);
+	}
+
+	public static Credential of(String token, String refreshToken) {
+		return new Credential(token, refreshToken);
+	}
 
 }
