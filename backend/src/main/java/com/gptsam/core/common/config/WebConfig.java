22a32d4dd0d51c471b8816ba6fd988839fa9b5eb
wsk0715 <58899677+wsk0715@users.noreply.github.com>
2025-03-24 19:11:26 +0900

    :sparkles:feat: BE - 로깅 도구 구현 (#35)

--- backend/src/main/java/com/gptsam/core/common/config/WebConfig.java
+++ backend/src/main/java/com/gptsam/core/common/config/WebConfig.java
@@ -1,18 +1,21 @@
 package com.gptsam.core.common.config;
 
 import com.gptsam.core.auth.resolver.LoginUserArgumentResolver;
+import com.gptsam.core.common.log.interceptor.RequestInterceptor;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+	private final RequestInterceptor requestInterceptor;
 	private final LoginUserArgumentResolver loginUserArgumentResolver;
 
 	@Value("${cors.allowed-origins}")
@@ -25,6 +28,11 @@ public class WebConfig implements WebMvcConfigurer {
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(true);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestInterceptor);
 	}
 
 	@Override
