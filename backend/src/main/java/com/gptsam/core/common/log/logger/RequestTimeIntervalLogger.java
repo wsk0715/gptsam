76b1819d39ab03f00c701c1e4559e22fbb37c0d8
wsk0715 <leindey@gmail.com>
2025-03-24 17:37:54 +0900

    :sparkles:feat: 요청 시간 측정 로거 추가(RequestTimeIntervalLogger)

---
+++ backend/src/main/java/com/gptsam/core/common/log/logger/RequestTimeIntervalLogger.java
@@ -0,0 +1,24 @@
+package com.gptsam.core.common.log.logger;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RequestTimeIntervalLogger {
+
+	private final ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+
+	public void start() {
+		startTime.set(System.currentTimeMillis());
+	}
+
+	public void log(String methodName, String requestUri) {
+		long duration = System.currentTimeMillis() - startTime.get();
+		log.info("[API Request Time] {} {} took {} ms", methodName, requestUri, duration);
+		startTime.remove();
+	}
+
+}
