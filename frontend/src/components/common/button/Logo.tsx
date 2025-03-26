3ef2d5bf188b406e1817574f062f19032dd8a42f
wsk0715 <leindey@gmail.com>
2025-03-27 04:30:24 +0900

    :bug:fix: 글자 크기 동적으로 설정되지 않던 점 수정
    - font size: 빌드 타임에 값이 정해지는 클래스네임 대신 런타임에 값이 정해지는 스타일 속성으로 변경

--- frontend/src/components/common/button/Logo.tsx
+++ frontend/src/components/common/button/Logo.tsx
@@ -2,12 +2,25 @@ import Link from 'next/link';
 
 interface LogoProps {
 	size: number;
+	isLink?: boolean;
 }
 
-export default function Logo({ size }: LogoProps) {
+export default function Logo({ size, isLink = true }: LogoProps) {
+	const style = {
+		fontSize: `${size}rem`,
+	};
+
 	return (
-		<Link href="/" className={`text-[${size}rem] font-bold`}>
-			GptSam
-		</Link>
+		<>
+			{isLink ? (
+				<Link href="/" className={`font-bold`} style={style}>
+					GptSam
+				</Link>
+			) : (
+				<div className={`font-bold`} style={style}>
+					GptSam
+				</div>
+			)}
+		</>
 	);
 }
